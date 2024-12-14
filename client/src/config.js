a71584952a75a4685913ea1541f40b725c7a056f
Aadarsh <as7128@srmist.edu.in>
2024-12-14 17:57:05 +0530

    Updated

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,6 @@
 const config = {
-    hostedUrl: "http://localhost:3003",
+    hostedUrl: "https://ediglobe-backend-main.onrender.com",
     localUrl: "http://localhost:3003",
-    // hostedUrls: "https://ediglobe-backend-main.onrender.com",
   };
   
   export default config;
